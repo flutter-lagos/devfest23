35817def48bb764881fb954661e6822dd894f1de
Iyinoluwa <dairoiyin@gmail.com>
2023-09-21 18:31:38 +0100

    fix shell route

--- lib/features/home/pages/agenda.dart
+++ lib/features/home/pages/agenda.dart
@@ -230,11 +230,11 @@ class _AgendaPageState extends ConsumerState<AgendaPage> {
                       name: 'Samuel Abada',
                       shortInfo: 'Senior Mobile Engineer, Cruise Nation',
                       onTap: () {
-                       // context.go( '/app/${TabItem.speakers.name}/${DevfestDay.day1.name}/speakers/$index');
-                       context.push(
+                        
+                        context.push(
                             '/app/${TabItem.speakers.name}/${DevfestDay.day1.name}');
-                      context
-                            .pushReplacement('/speakers/${TabItem.speakers.name}/$index');
+                        context.pushReplacement(
+                            '/speakers/${TabItem.speakers.name}/$index');
                       },
                     );
                   },
