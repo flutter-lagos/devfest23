77e738b75103a1d877e481969f07defccf340d3d
Iyinoluwa <dairoiyin@gmail.com>
2023-10-26 21:39:41 +0100

    minor change

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
